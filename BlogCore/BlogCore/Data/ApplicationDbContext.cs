2adf73087c7be43c7babee0513642e5a624c00ac
Josue <josuegzariett@gmail.com>
2025-10-15 23:21:53 -0600

    Initial project setup for BlogCore
    Added solution and project files for BlogCore, including data access, models, utilities, and main web project. Set up ASP.NET Core MVC structure with Identity, Entity Framework, controllers, views, static assets, and configuration files for development and database connection.

---
+++ BlogCore/BlogCore/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogCore.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        // TODOS LOS MODELOS QUE SE DESEAN CREAR
+    }
+}
