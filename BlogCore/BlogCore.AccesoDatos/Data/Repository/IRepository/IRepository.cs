81bb73ce2b8f5297db208f66f364bd78ee8c54a6
Josue <josuegzariett@gmail.com>
2025-10-29 16:29:59 -0600

    Add generic repository interface and base implementation
    Introduces IRepository<T> interface and a base Repository<T> class for generic data access. The Repository class provides basic Add and Get methods, with other methods stubbed for future implementation.

---
+++ BlogCore/BlogCore.AccesoDatos/Data/Repository/IRepository/IRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogCore.AccesoDatos.Data.Repository.IRepository
+{
+    // T es un parametro de tipo generico que representa la entidad con la que se va a trabajar (class, interfaz o metodo)
+    public interface IRepository<T> where T : class
+    {
+        T Get(int id);
+
+        IEnumerable<T> GetAll(
+            Expression<Func<T, bool>>? filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>>? orderyBy = null,
+            string? includeProperties = null
+            );
+        T GetFirstOrDefault(
+            Expression<Func<T, bool>>? filter = null,
+            string? includeProperties = null
+            );
+        void Add(T entity);
+        void Remove(int id);
+        void Remove(T entity);
+    }
+}
