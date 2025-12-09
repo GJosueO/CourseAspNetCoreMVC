89ae9a9442941ee5284eb75a3c6c889bc6a586e8
Josue <josuegzariett@gmail.com>
2025-12-09 16:13:54 -0600

    Add Categoria repository and interface
    Introduces CategoriaRepository and ICategoriaRepository for managing Categoria entities. Includes a specific Update method and a method to retrieve all categories ordered by the 'Orden' field.

---
+++ BlogCore/BlogCore.AccesoDatos/Data/Repository/CategoriaRepository.cs
@@ -0,0 +1,29 @@
+﻿using BlogCore.AccesoDatos.Data.Repository.IRepository;
+using BlogCore.Data;
+using BlogCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogCore.AccesoDatos.Data.Repository
+{
+    internal class CategoriaRepository : Repository<Categoria>, ICategoriaRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoriaRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Update(Categoria categoria) // Solo se crea un metodo Update en el repositorio de Categoria porque es una operacion especifica que no esta cubierta por los metodos genericos del repositorio base
+        {
+            var objDesdeDb = _db.Categoria.FirstOrDefault(s => s.Id == categoria.Id);// BUSCAMOS LA CATEGORIA EN LA BASE DE DATOS UTILIZANDO SU ID
+            objDesdeDb.Nombre =  categoria.Nombre;// ACTUALIZAMOS EL NOMBRE DE LA CATEGORIA
+            objDesdeDb.Orden = categoria.Orden;// ACTUALIZAMOS EL ORDEN DE LA CATEGORIA
+
+            _db.SaveChanges();// GUARDAMOS LOS CAMBIOS EN LA BASE DE DATOS
+        }
+    }
+}
---
+++ BlogCore/BlogCore.AccesoDatos/Data/Repository/IRepository/ICategoriaRepository.cs
@@ -0,0 +1,15 @@
+﻿using BlogCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogCore.AccesoDatos.Data.Repository.IRepository
+{
+    public interface ICategoriaRepository : IRepository<Categoria>
+    {
+        void Update(Categoria categoria); // EL REPOSITORIO UPDATE SE IMPLEMENTA DE MANERA ESPECIFICA EN ICATEGORIA YA QUE IDENTITYFRAMEWORK NO PROPORCIONA UN METODO UPDATE PREDEFINIDO, POR QUE ES UNA TECNOLOGIA ORM (OBJECT RELATIONAL MAPPING) QUE SE ENCARGA DE MAPEAR LAS ENTIDADES DE LA APLICACION A LAS TABLAS DE LA BASE DE DATOS Y PROPORCIONA METODOS PARA REALIZAR OPERACIONES CRUD (CREAR, LEER, ACTUALIZAR, ELIMINAR) EN LA BASE DE DATOS.
+        IEnumerable<Categoria> GetAllCategoriasOrdenadas(); // METODO PARA OBTENER TODAS LAS CATEGORIAS ORDENADAS POR EL CAMPO ORDEN
+    }
+}
